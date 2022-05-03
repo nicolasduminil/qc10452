bad27925362ce2dae0f99f832b1f5b9fd184480b
Nicolas DUMINIL <nicolas.duminil@simplex-software.fr>
2022-05-03 15:07:53 +0200

    Have removed Iqc10452

--- qc10452-client/src/main/java/com/grdf/poc/Iqc10452.java
+++
@@ -1,8 +0,0 @@
-package com.grdf.poc;
-
-public interface Iqc10452
-{
-  public String qc10452Literal(String data);
-  public String qc10452Escaped(String data);
-  public String qc10452Unescaped(String data);
-}
--- qc10452-client/src/main/java/com/grdf/poc/Qc10452Bean.java
+++ qc10452-client/src/main/java/com/grdf/poc/Qc10452Bean.java
@@ -1,28 +1,8 @@
 package com.grdf.poc;
 
-import javax.ejb.*;
-import javax.xml.namespace.*;
-import javax.xml.ws.*;
+import javax.enterprise.context.*;
 
-@Stateless
-@Local(Iqc10452.class)
-public class Qc10452Bean implements Iqc10452
+@Dependent
+public class Qc10452Bean
 {
-  @WebServiceRef(type=Qc10452Service.class)
-  private Qc10452Service service;
-
-  public String qc10452Literal(String data)
-  {
-    return service.getQc10452Port().qc10452Literal(data);
-  }
-
-  public String qc10452Escaped(String data)
-  {
-    return service.getQc10452Port().qc10452Escaped(data);
-  }
-
-  public String qc10452Unescaped(String data)
-  {
-    return service.getQc10452Port().qc10452Unescaped(data);
-  }
 }
